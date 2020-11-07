27622952ab913568b868905e660e2e38d22df983
Andrew <orcahmlee@gmail.com>
2020-11-07 17:13:18 +0800

    - re-organized

--- cli/argparse/simple.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-
-if __name__ == "__main__":
-    for i in range(len(sys.argv)):
-        print(sys.argv[i])
--- cython/run_as_python.py
+++
@@ -1,5 +0,0 @@
-def factorial(n):
-    ans = 1
-    for i in range(1, n + 1):
-        ans *= i
-    return ans
