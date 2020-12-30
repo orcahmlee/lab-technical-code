7f23028e8f3d12d52ecc13bee7d5116e16e89b6c
Andrew <orcahmlee@gmail.com>
2020-12-30 16:46:09 +0800

    - update

--- DevOps/nginx-uwsgi-django/depoly-at-root/dj3/dj3/settings.py
+++ DevOps/nginx-uwsgi-django/depoly-at-root/dj3/dj3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!^-p96ces65=nkgedrbvc2v5^y6asmm5#y#-3+&u7edzju16pu'
+SECRET_KEY = os.getenv('SECRET_KEY', '!^-p96ces65=nkgedrbvc2v5^y6asmm5#y#-3+&u7edzju16pu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
